61f705cd371c28b4611eeab267b43192520b0435
thals7 <thals_7@naver.com>
2020-09-07 00:54:08 +0900

    solve 2438

---
+++ baekjoon/for문/2438.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n):
+    for j in range(i):
+        print('*', end='')
+    print('*')
+
+
+for i in range(1, 6):
+    for j in range(i):
+        print('*', end='')
+    print()

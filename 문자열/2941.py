59cd044ebe8e8896019dd76e95f19c1fa287bdab
thals7 <thals_7@naver.com>
2020-10-10 16:33:01 +0900

    2941 ing

---
+++ 문자열/2941.py
@@ -0,0 +1,13 @@
+Croa = ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
+Word = str(input())
+Cnt = 0
+for i in range(len(Word)):
+    if Word[i:i+2] in Croa:
+        if i <= len(Word)-3:
+            if Word[i:i+2] == 'dz' and Word[i+2] != '=':
+                Cnt += 1
+        else:
+            continue
+    else:
+        Cnt += 1
+print(Cnt)

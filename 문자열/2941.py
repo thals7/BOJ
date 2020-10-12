fc293347cf48d7358621a2a7089b297b0d5f3854
thals7 <thals_7@naver.com>
2020-10-13 01:57:14 +0900

    solve 1712

--- 문자열/2941.py
+++ 문자열/2941.py
@@ -1,14 +1,14 @@
-Croa = ['c=', 'c-', 'dz', 'd-', 'lj', 'nj', 's=', 'z=']
-Word = str(input())
+Croa = ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
+Word = input()
 Cnt = 0
 for i in range(len(Word)):
-    print(Word[i:i+2])
     if Word[i:i+2] in Croa:
-        if i <= len(Word)-3:
-            if Word[i:i+2] == 'dz' and Word[i+2] != '=':
-                Cnt += 1
+        if Word[i-1] == 'd':
+            Cnt -= 1
+            continue
         else:
             continue
     else:
         Cnt += 1
 print(Cnt)
+

a5592bfa581c4176a647ee8a24ae034c965f9548
thals7 <thals_7@naver.com>
2021-01-27 11:28:10 +0900

    solve 11279

---
+++ 우선순위 큐/11279.py
@@ -0,0 +1,14 @@
+import sys, heapq
+
+n = int(sys.stdin.readline().rstrip())
+heap = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline().rstrip())
+    if x == 0:
+        if len(heap) == 0:
+            sys.stdout.write("0"+"\n")
+        else:
+            sys.stdout.write(str(heapq.heappop(heap)[1])+"\n")
+    else:
+        heapq.heappush(heap,(-x,x))

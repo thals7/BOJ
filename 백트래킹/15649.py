2b87a01efd59f24d727f6570c411c4b529d9b716
thals7 <thals_7@naver.com>
2021-02-14 23:30:38 +0900

    solve 15649

---
+++ 백트래킹/15649.py
@@ -0,0 +1,29 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+n,m = map(int,input().split())
+
+num = [i+1 for i in range(n)]
+visited = [False] * n
+ans = []
+
+def dfs(cnt):
+    if cnt == m:
+        for i in range(m):
+            print(ans[i], end=' ')
+        print()
+        return
+
+    for i in range(n):
+        if visited[i]:
+            continue
+        ans.append(num[i])
+        visited[i] = [True]
+
+        dfs(cnt+1)
+
+        ans.pop()
+        visited[i] = False
+
+dfs(0)

bb481254edc47337b4f46fe1d129f3c47c28bb5a
thals7 <thals_7@naver.com>
2021-04-12 00:19:46 +0900

    solve 2839

--- 수학1/2839.py
+++ 수학1/2839.py
@@ -1,18 +1,18 @@
-N = int(input())
-if N%5 == 0:
-    print(N//5)
-elif N%5 > 0:
-    if N%5 == 1:
-        print(N//5-1 + 2)
-    elif N>7 and N%5 == 2:
-        print(N//5-2 + 4)
-    elif N%5 == 3:
-        print(N//5 + 1)
-    elif N>4 and N%5 == 4:
-        print(N//5-1 + 3)
-    else:
+import sys
 
-elif N == 3:
-    print(1)
-else:
+n = int(sys.stdin.readline())
+
+five = 0
+three = 0
+
+while n > 0:
+    if n % 5 == 0:
+        five += n // 5
+        break
+    three += 1
+    n -= 3
+
+if n < 0:
     print(-1)
+else:
+    print(five+three)

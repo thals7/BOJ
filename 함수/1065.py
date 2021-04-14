48a1b49d1804e00ac0daedb1ce37848f679c3481
thals7 <thals_7@naver.com>
2021-04-14 21:33:04 +0900

    solve 1065

--- 함수/1065.py
+++ 함수/1065.py
@@ -1,4 +1,23 @@
-N = str(input())
+import sys
 
-for i in range(1,N+1):
-    for j in len(str(i)):
+n = int(sys.stdin.readline())
+cnt = 0
+
+if n > 99:
+    cnt = 99
+    for i in range(100,n+1):
+        num = list(map(int,list(str(i))))
+        isSeq = True
+        tmp = num[-1]-num[-2]
+        for j in range(len(num)-1,0,-1):
+            if tmp == num[j]-num[j-1]:
+                continue
+            else:
+                isSeq = False
+                break
+        if isSeq:
+            cnt += 1
+else:
+    cnt = n
+
+print(cnt)

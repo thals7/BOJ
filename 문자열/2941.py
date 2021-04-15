522ad9e13fa6572154f5082184ec3f2187a95e96
thals7 <thals_7@naver.com>
2021-04-15 23:18:52 +0900

    solve 2941

--- 문자열/2941.py
+++ 문자열/2941.py
@@ -1,14 +1,29 @@
-Croa = ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
-Word = input()
-Cnt = 0
-for i in range(len(Word)):
-    if Word[i:i+2] in Croa:
-        if Word[i-1] == 'd':
-            Cnt -= 1
-            continue
-        else:
-            continue
-    else:
-        Cnt += 1
-print(Cnt)
+import sys
 
+# 1. 처음 풀이
+croa = list(sys.stdin.readline().rstrip())
+cnt = 0
+
+for i in range(len(croa)):
+    cnt += 1
+    if croa[i] == '=':
+        if i > 1 and croa[i-1] == 'z' and croa[i-2] == 'd':
+            cnt -= 2
+        else:
+            cnt -= 1
+    elif croa[i] == '-':
+        cnt -= 1
+    elif i > 0 and croa[i] == 'j':
+        if croa[i-1] == 'l' or croa[i-1] == 'n':
+            cnt -= 1
+
+print(cnt)
+
+
+# 2. replace 함수 활용
+word = sys.stdin.readline().rstrip()
+croa = ['c=','c-','d-','dz=','lj','nj','s=','z=']
+
+for i in croa:
+    word = word.replace(i,' ')
+print(len(word))
